036d825c893526859ad14aa296faac12537597cf
MinexAutomation <minex.automation@gmail.com>
2020-05-09 17:48:37 -0700

    Added extensions for Typed<T>.

---
+++ source/R5T.Amphipolis/Code/Extensions/TypedExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.Amphipolis
+{
+    public class TypedExtensions
+    {
+        public static bool ValueEquals<T>(Typed<T> a, Typed<T> b)
+        {
+            var valueEquals = a.Value.Equals(b.Value);
+            return valueEquals;
+        }
+    }
+}
