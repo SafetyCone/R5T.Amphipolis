e000043835820e5a7e4a4107dc04f44dbedc05e7
MinexAutomation <minex.automation@gmail.com>
2020-05-09 17:38:52 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Amphipolis.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Amphipolis.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
